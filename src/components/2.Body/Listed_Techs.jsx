05ea39271556dfdceedd5d152a819779323cad49
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-08-22 19:03:52 +0100

    addNextImageToDisplay fixed

--- src/components/2.Body/Listed_Techs.jsx
+++ src/components/2.Body/Listed_Techs.jsx
@@ -9,20 +9,41 @@ export const ListedTechs = (props) =>
     const [currentDisplayedImages, setCurrentDisplayedImages] = useState(imageArray.slice(0, 12));
 
     /*
-        function to be called when you want to add a new image to the end of the state that contains all current images
+        function to be called when you want to add a new image to the end of the state that contains all currently displayed images
         
-        first it queries the current length of the current state of the displayed images and determines the index by
-        taking the remainder of it after dividing by the length of the full image array. This automatically cyles the index
-        back to 0 when you reach the end of the array
+        checks the url of the last image of the current display against the entire imageArray and if they're equivalent appends the
+        next image (if its the last image in the imageArray thats equivalent instead appends the first image)
 
         it then sets the state to the previous array with the appended image
     */
     function addNextImageToDisplay()
     {
+        let nextIndexFromFullArray = 0; //the index of the image we need to pull from the array (0 set as default)
+
+        /*
+            checks the last image of the display against all images in the image array, 
+            if its equal to the last one sets the next image to pull from the imageArray to be 0, 
+            otherwise gets the next image from the image array after the one in the display
+        */ 
+        for (i = 0; i < imageArray.length; i++)
+        {
+            if (currentDisplayedImages[currentDisplayedImages.length - 1] == imageArray[i] && i != imageArray.length - 1)//if URL is matched then assign index and break
+            {
+                if (i != imageArray.length - 1)
+                {
+                    nextIndexFromFullArray = i + 1;
+                }
+                else
+                {
+                    nextIndexFromFullArray = 0;
+                }
+                break;
+            }
+        }
+
         setCurrentDisplayedImages(
             previousStateImageArray =>
             {
-                const nextIndexFromFullArray = previousStateImageArray.length % imageArray.length;
                 return [...previousStateImageArray, imageArray[nextIndexFromFullArray]];
             }
 
