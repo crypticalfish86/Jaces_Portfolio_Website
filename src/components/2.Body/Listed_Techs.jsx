d741c02979f44523e70d227a2e5de8ab34dd53a7
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-08-25 16:49:38 +0100

    successfully got filenames as titles from path

--- src/components/2.Body/Listed_Techs.jsx
+++ src/components/2.Body/Listed_Techs.jsx
@@ -1,22 +1,33 @@
 export const ListedTechs = (props) =>
 {
     const imageArray = props.imageArray; //imageArray from parent containing all images of techs you've worked on
+    const imageTitles = props.imageTitles;//names of the images
+    
 
     return(
         <div className="Listed_Techs_Container">
             <div className="Tech_Images_Display">
                 {imageArray.map((image, index) => (
-                    <img key={index} src={image} alt={`img-${index}`}  className="Tech_Image"/>
+                    <div className="Tech_Image_Box_Container">
+                        {imageTitles[index]} 
+                        <img key={index} src={image} alt={`img-${index}`}  className="Tech_Image"/>
+                    </div>
                 ))}
             </div>
             <div className="Tech_Images_Display">
                 {imageArray.map((image, index) => (
+                <div className="Tech_Image_Box_Container"> 
+                    {imageTitles[index]}
                     <img key={index} src={image} alt={`img-${index}`}  className="Tech_Image"/>
+                </div>
                 ))}
             </div>
             <div className="Tech_Images_Display">
                 {imageArray.map((image, index) => (
+                <div className="Tech_Image_Box_Container">
+                    {imageTitles[index]}  
                     <img key={index} src={image} alt={`img-${index}`}  className="Tech_Image"/>
+                </div>
                 ))}
             </div>
         </div>
--- src/components/2.Body/Listed_Techs_Container.jsx
+++ src/components/2.Body/Listed_Techs_Container.jsx
@@ -3,16 +3,26 @@ import { ListedTechs } from "./Listed_Techs";
 /*Purpose of this component is to get a list of all images once and not need to get them again ever render of ListedTechs*/
 export const ListedTechsContainer = () =>
 {
-        /*Function to import all images from a specific path and map them onto an array*/
+        /*Function to return an object that imports all images from a specific folder and map them onto an array as well as the filenames of the files*/
         function importAll(path)
         {
-            return path.keys().map(path);
+            const images = path.keys().map((item) =>{
+                const filename = item.replace('./', '');
+                return {
+                    image: path(item),//imported image
+                    fileName: filename //the filename
+                }
+            })
+            return images
         }
     
-        const imageArray = importAll(require.context('../../Assets/Technology_Images', false, /\.(png|jpe?g|svg)$/))
+        let imageFilesArray = importAll(require.context('../../Assets/Technology_Images', false, /\.(png|jpe?g|svg)$/)) //import all pngs, jpgs, jpegs and svgs from this path
+        const imageNames = imageFilesArray.map(image => {return image.fileName}) //get an array of file names
+        const imageTitles = imageNames.map(name => {return name.replace(/^\d+|(?<=\.)[^.]*$/g, '')}) //turns those filenames and turn them into their actual titles
+        const imageArray = imageFilesArray.map(image => {return image.image}) //get an array of just the images
         return(
             <div>
-                <ListedTechs imageArray= {imageArray} />
+                <ListedTechs imageArray= {imageArray} imageTitles={imageTitles} />
             </div>
         )
 }
